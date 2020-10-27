a6642b6e79e3ca657ab6e9715cf719ab5b35d69c
Fahid Khan <fahid.khan@tuni.fi>
2020-10-27 20:59:31 +0200

    Testing:

--- src/Helpers/utils.js
+++ src/Helpers/utils.js
@@ -11,3 +11,5 @@ const date = new Date();
 export const Month = Months[date.getMonth()];
 export const Weekday = Weekdays[date.getDay()];
 export const Day = date.getDate();
+//Hello Fahid
+//hello man
